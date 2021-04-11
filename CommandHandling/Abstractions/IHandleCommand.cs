c9aac554ea2628c441cb61309031b0bde74180c0
moaliba <Mohammad.Ali.Barati@Gmail.com>
2021-04-11 14:22:29 +0430

    Complete Command Handleling Infrastracture

---
+++ CommandHandling/Abstractions/IHandleCommand.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace CommandHandling.Abstractions
+{
+    public interface IHandleCommand<Tcommand> where Tcommand : Acommand
+    {
+        public Task Handle(Tcommand command);
+    }
+}
