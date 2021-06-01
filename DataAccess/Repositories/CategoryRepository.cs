931c5198db78c926b33ddf7f6a255c02cfa67294
moaliba <Mohammad.Ali.Barati@Gmail.com>
2021-06-01 16:22:57 +0430

    Add repository For Category (Insert, Update, Delete, Find)

---
+++ DataAccess/Repositories/CategoryRepository.cs
@@ -0,0 +1,40 @@
+﻿using DomainModel;
+using EventHandling.Abstractions;
+using System;
+using System.Linq;
+using UseCases.RepositoryContracts;
+
+namespace DataAccess.Repositories
+{
+    public class CategoryRepository : Repository, ICategoryRepository
+    {
+        readonly IEventBus eventBus;
+        public CategoryRepository(IWriteDbContext dbContext, IEventBus eventBus) : base(dbContext)
+        => this.eventBus = eventBus;
+
+        public void Create(Category category)
+        {
+            foreach (AnEvent @event in category.Events)
+            {
+                eventBus.Publish(@event);
+            }
+            category.ClearEvents();
+            dbContext.Categories.Add(category);
+        }
+
+        public bool DeosExist(string title)
+        => dbContext.Categories.FirstOrDefault(c => c.Title == title) != null;
+
+        public Category Find(Guid Id)
+        => dbContext.Categories.FirstOrDefault(c => c.Id == Id);
+
+        public void Update(Category category)
+        {
+            foreach (AnEvent @event in category.Events)
+            {
+                eventBus.Publish(@event);
+            }
+            category.ClearEvents();
+        }
+    }
+}
