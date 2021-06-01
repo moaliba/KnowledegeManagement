8c3108e4b4024798ab1ea3541cbd6eee4cdb90ac
marzi-karani <marzieh.karani@gmail.com>
2021-06-01 11:18:47 +0430

    Add Category Test

---
+++ DomainModel.Tests/DomainModel.Tests/Addsnewcategory.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+using FluentAssertions;
+using KnowledgeManagement;
+
+namespace KnowledgeManagement
+{
+    public class AddsnewcategorySpec
+    {
+        [Fact]
+        public void Addsnewcategory()
+        {
+            var category = Category.Definecategory("1", "Programming");
+            category.Events.Should().ContainEquivalentOf(
+                    new Categorydefined("1", "Programming")
+                );
+        }
+    }
+}
