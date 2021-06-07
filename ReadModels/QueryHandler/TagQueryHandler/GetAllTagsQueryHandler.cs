1ac9c0b2fe3a12c90d88d5f97af746de506f193c
marzi-karani <marzieh.karani@gmail.com>
2021-06-07 11:50:41 +0430

    paging and sorting in getAllTags

--- ReadModels/QueryHandler/TagQueryHandler/GetAllTagsQueryHandler.cs
+++ ReadModels/QueryHandler/TagQueryHandler/GetAllTagsQueryHandler.cs
@@ -14,35 +14,41 @@ namespace ReadModels.QueryHandler.TagQueryHandler
         public GetAllTagsQueryHandler(IReadDbContext dbContext)
         => this.dbContext = dbContext;
         
-        public async Task<TagViewModelList> Handle(GetAllTagsQuery query)
+        public  Task<TagViewModelList> Handle(GetAllTagsQuery query)
         {
-            var result = dbContext.TagViewModels
-            .Where(t => t.Title.Contains(query.Title ?? string.Empty) && t.IsActive==true && t.CategoryId == query.CategoryId) //
-            .Skip((query.PageNumber - 1) * query.PageSize)
-            .Take(query.PageSize);
+            var TotalItems = dbContext.TagViewModels
+            .Where(t => t.Title.Contains(query.Title ?? string.Empty) && t.IsActive == true && (query.CategoryId ==null || t.CategoryId == query.CategoryId )); //
+            //.Skip((query.PageNumber - 1) * query.PageSize)
+            //.Take(query.PageSize);
 
             switch (query.SortOrder)
             {
                 case "Title":
-                    result = result.OrderBy(t => t.Title);
+                    TotalItems = TotalItems.OrderBy(t => t.Title);
                     break;
                 case "Title_desc":
-                    result = result.OrderByDescending(t => t.Title);
+                    TotalItems = TotalItems.OrderByDescending(t => t.Title);
                     break;
                 case "Category":
-                    result = result.OrderBy(t => t.CategoryId);
+                    TotalItems = TotalItems.OrderBy(t => t.CategoryId);
                     break;
                 case "Category_desc":
-                    result = result.OrderByDescending(t => t.CategoryId);
+                    TotalItems = TotalItems.OrderByDescending(t => t.CategoryId);
                     break;
                 default:
                     break;
             }
 
-            var totalRecords = await dbContext.TagViewModels.Where(t => t.Title.Contains(query.Title ?? string.Empty) && t.IsActive == true && t.CategoryId == query.CategoryId).CountAsync();
+            var totalRecords = TotalItems.Count();
+
+           // var totalRecords = await dbContext.TagViewModels.CountAsync(t => t.Title.Contains(query.Title ?? string.Empty) && t.IsActive == true && t.CategoryId == query.CategoryId);
+
+            var result=TotalItems
+                .Skip((query.PageNumber - 1) * query.PageSize)
+                .Take(query.PageSize);
 
             var tagResult = new TagViewModelList() { TagViewModels = result.AsEnumerable(), TotalCount = totalRecords };
-            return tagResult;
+            return Task.FromResult(tagResult);
 
         }
     }
