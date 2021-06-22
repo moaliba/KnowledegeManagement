8fa524e9d8dcf845fc14a8e23fe2ba6e82676fc7
marzi-karani <marzieh.karani@gmail.com>
2021-06-22 10:52:56 +0430

    Refactoring

--- ReadModels/QueryHandler/TagQueryHandler/GetAllTagsQueryHandler.cs
+++ ReadModels/QueryHandler/TagQueryHandler/GetAllTagsQueryHandler.cs
@@ -17,27 +17,16 @@ namespace ReadModels.QueryHandler.TagQueryHandler
         public  Task<PagedViewModel<TagViewModel>> Handle(GetAllTagsQuery query)
         {
             var TotalItems = dbContext.TagViewModels
-            .Where(t => t.Title.Contains(query.Title ?? string.Empty)  && (query.CategoryId ==null || t.CategoryId == query.CategoryId )); 
+            .Where(t => t.Title.Contains(query.Title ?? string.Empty)  && (query.CategoryId ==null || t.CategoryId == query.CategoryId ));
 
-            switch (query.SortOrder)
+            TotalItems = query.SortOrder switch
             {
-                case "title":
-                    TotalItems = TotalItems.OrderBy(t => t.Title);
-                    break;
-                case "title_desc":
-                    TotalItems = TotalItems.OrderByDescending(t => t.Title);
-                    break;
-                case "category":
-                    TotalItems = TotalItems.OrderBy(t => t.CategoryName);
-                    break;
-                case "category_desc":
-                    TotalItems = TotalItems.OrderByDescending(t => t.CategoryName);
-                    break;
-                default:
-                    TotalItems = TotalItems.OrderBy(t => t.InsertDate);
-                    break;
-            }
-
+                "title" => TotalItems.OrderBy(t => t.Title),
+                "title_desc" => TotalItems.OrderByDescending(t => t.Title),
+                "category" => TotalItems.OrderBy(t => t.CategoryName),
+                "category_desc" => TotalItems.OrderByDescending(t => t.CategoryName),
+                _ => TotalItems.OrderBy(t => t.InsertDate),
+            };
             var totalRecords = TotalItems.Count();
 
                 var result = PagingUtility.Paginate(query.PageNumber, query.PageSize, TotalItems);
